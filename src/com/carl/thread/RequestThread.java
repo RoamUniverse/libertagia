9ab14bd6919e153921a45ac5d4213a7e6dd3979f
carl.huang <carl.huang@H3000-0236.perficient.com>
2014-05-29 16:50:29 +0800

    t

--- src/com/carl/thread/RequestThread.java
+++ src/com/carl/thread/RequestThread.java
@@ -10,6 +10,7 @@ import com.carl.pojo.UserInfo;
 public class RequestThread extends Thread {
 	private MainController controller;
 	private UserInfo userInfo;
+	private boolean isInit = false;
 
 	public RequestThread(MainController controller, UserInfo userInfo) {
 		super();
@@ -17,16 +18,33 @@ public class RequestThread extends Thread {
 		this.userInfo = userInfo;
 	}
 
+	public RequestThread(MainController controller, UserInfo userInfo,
+			boolean isInit) {
+		super();
+		this.controller = controller;
+		this.userInfo = userInfo;
+		this.isInit = isInit;
+	}
+
 	public void run() {
-		try {
-			Request.getInitCookiesAndCaptcha(userInfo);
-			controller.updateTable(userInfo);
-			controller.showLogs(LogLevel.LOG_INFO,
-					"用户名:" + userInfo.getUsername() + "\t初始化完成.");
-		} catch (IOException e) {
-			controller.showLogs(LogLevel.LOG_ERROR,
-					"用户名:" + userInfo.getUsername() + "\t初始化状态错误,请检查网络.");
+		if (isInit) {
+			try {
+				Request.getInitCookiesAndCaptcha(userInfo);
+				controller.updateTable(userInfo);
+				controller.showLogs(LogLevel.LOG_INFO,
+						"用户名:" + userInfo.getUsername() + "\t初始化完成.");
+			} catch (IOException e) {
+				controller.showLogs(LogLevel.LOG_ERROR,
+						"用户名:" + userInfo.getUsername() + "\t初始化状态错误,请检查网络.");
+			}
+		}else {
+			
 		}
+		
+	}
+	
+	private void startWork() {
+		
 	}
 
 	public void setController(MainController controller) {
